e0948cab1c2135edb5884ecc61f6ffaf115a0e80
Ari Permana Putra <ari.putra@gdn-commerce.com>
2023-03-06 18:10:59 +0700

    save message by username and add exception handling

--- src/services/index.js
+++ src/services/index.js
@@ -2,56 +2,75 @@ const db = require('../models')
 const jwt = require('jsonwebtoken')
 
 const processLogin = async(req, res) => {
-    const {username, password} = req.body;
+    try {
+        const {username, password} = req.body;
 
-    await db.select('username', 'pwd').from('test_users')
-    .where('username', '=', username)
-    .andWhere('pwd', '=', password)
-    .then(user => {
-        if (!user[0])
-            return res.status(401).send({message: "UNAUTHORIZED"});
+        await db.select('username', 'pwd').from('test_users')
+        .where('username', '=', username)
+        .andWhere('pwd', '=', password)
+        .then(user => {
+            if (!user[0])
+                return res.status(401).send({message: "UNAUTHORIZED"});
 
-        const token = jwt.sign({
-            username
-        }, 'secret', {expiresIn: 3600000})
+            const token = jwt.sign({
+                username
+            }, 'secret', {expiresIn: 3600000})
 
-        return res.status(200).send({
-            accessToken: token
-        })  
-    })
+            return res.status(200).send({
+                accessToken: token
+            })  
+        })
+    } catch(err) {
+        throw new Error(err.message);
+    }
 }
 
 const processInsert = async(req, res) => {
-    const {message} = req.body;
+    try {
+        const { message } = req.body;
+        const username = req.jwtUsername;
 
-
-    db.transaction(trx => {
-        trx.insert({
-            message: message
+        await db.transaction(trx => {
+            trx.insert({
+                message: message,
+                username: username
+            })
+            .into('messages')
+            .returning('*')
+            .then(result => {
+                res.status(200).send({
+                    message: 'Message succesfully saved!'
+                }) 
+            })
+            .then(trx.commit)
+            .catch(err => {
+                trx.rollback
+                throw new Error(err.message);
+            });
         })
-        .into('messages')
-        .returning('*')
-        .then(result => {
-            res.status(200).send({
-                message: 'Message succesfully saved!'
-            }) 
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => {
-        console.log(err.message);
+        .catch(err => {
+            throw new Error(err.message);
+        });
+    } catch(err) {
         throw new Error(err.message);
-    });
+    }
 }
 
 const processGet = async(req, res) => {
-    await db.select('message').from('messages')
-    .then(messages => {
-        return res.status(200).send({
-            data: messages
-        })  
-    })
+    try {
+        const username = req.jwtUsername;
+        
+        await db.select('message')
+        .from('messages')
+        .where('username', '=', username)
+        .then(messages => {
+            return res.status(200).send({
+                data: messages
+            })  
+        })
+    } catch(err) {
+        throw new Error(err.message);
+    }
 }
 
 module.exports = {
